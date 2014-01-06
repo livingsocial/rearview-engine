b7acdb825098427a159b4f150e8a1bed5ee6e61d
Trent Albright <trent.albright@gmail.com>
2014-01-06 13:55:21 -0500

    Release version 1.0.2.rc.3
    - minor performance tweak to job data retrieval

--- app/controllers/rearview/jobs_controller.rb
+++ app/controllers/rearview/jobs_controller.rb
@@ -37,7 +37,7 @@ module Rearview
     end
 
     def data
-      @job_data = Rearview::Job.find(params[:id]).try(:job_data)
+      @job_data = Rearview::JobData.find_by(:job_id,params[:id])
       unless @job_data.present?
         head :status => 404
       end
