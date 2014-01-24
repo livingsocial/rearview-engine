8acb9dcc86464cae404df90d4917a5bee27f131e
Trent Albright <trent.albright@gmail.com>
2014-01-24 13:55:24 -0500

    Remove initializer that was left in by mistake
    This initializer was intended to be left in the engine host, not in the
    engine.
    
    Fixes livingsocial/rearview#20

--- config/initializers/unlimited_strength_cryptography.rb
+++
@@ -1,5 +0,0 @@
-# Only for non-production environments. For production systems install the correct jurisdiction jars:
-# https://github.com/jruby/jruby/wiki/UnlimitedStrengthCrypto
-if !Rails.env.production?
-  java.lang.Class.for_name('javax.crypto.JceSecurity').get_declared_field('isRestricted').tap{|f| f.accessible = true; f.set nil, false}
-end
